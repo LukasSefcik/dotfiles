3e08b9f492fd5ab97b30e6af17ea6c7cd3a562df
Šefčík Lukáš <sefcik@kros.sk>
2025-05-06 11:05:57 +0200

    Update configs.

--- .config/sketchybar/items/cpu.sh
+++ .config/sketchybar/items/cpu.sh
@@ -2,7 +2,7 @@
 
 sketchybar --add item cpu right \
            --set cpu script="$PLUGIN_DIR/cpu.sh" \
-                     label.width=30 \
+                     label.width=35 \
                      update_freq=2 \
            --set cpu icon="$CPU" \
            --subscribe cpu system_woke
