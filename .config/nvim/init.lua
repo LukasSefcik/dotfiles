6425e04d24f893ab0595a86023e67c6e37a03793
Šefčík Lukáš <sefcik@kros.sk>
2025-06-27 14:10:02 +0200

    Update.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -885,7 +885,10 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        html = { 'prettier' },
+        css = { 'prettier' },
       },
     },
   },
